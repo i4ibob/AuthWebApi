2d7f5354c443778875a44874ec459b5956b75ef5
i4ibob <i4ibob@gmail.com>
2024-11-25 21:03:36 +0200

    Registration + login

---
+++ BL/Services/JwtService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace BL.Services
+{
+    class JwtService
+    {
+      public string GenetateToken(UserLoginService account)
+      {
+
+            var jwtToken = new
+       return 
+      }
+    }
+}
